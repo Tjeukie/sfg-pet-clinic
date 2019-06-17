9debc6f25e572667ef885bb8bfad502296fb7345
Tjeukie <37412778+Tjeukie@users.noreply.github.com>
2019-06-17 22:05:57 +0200

    Multi module outline. solves #2

--- pet-clinic-web/src/test/java/guru/springframework/petclinicweb/BasicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinicweb/BasicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package guru.springframework.petclinicweb;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BasicApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
