e7be07ea213712e237043a7f9631df2cb37d315d
Tjeukie <37412778+Tjeukie@users.noreply.github.com>
2019-07-08 18:29:08 +0200

    Convert Owners to JPA Entities. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/petclinicdata/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinicdata/model/Person.java
@@ -1,8 +1,18 @@
 package guru.springframework.petclinicdata.model;
 
+//Tells JPA we're going to inherit from this class
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+//Tells JPA we're going to inherit from this class
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    //Hibernate uses 'snake-case'
+    @Column(name = "first_name")
     private String firstName;
+    
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
@@ -24,6 +34,5 @@ public class Person extends BaseEntity {
         System.out.println("Person - setLastName");
         this.lastName = lastName;
     }
-    
-    
+
 }
